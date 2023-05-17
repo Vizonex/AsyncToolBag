fcee588ee65bdf4c0fa94380c9f36f8c57d86178
Vizonex <114684698+Vizonex@users.noreply.github.com>
2023-05-17 14:41:51 -0500

    Update events.py
    Modified Ratelimit class Object conditions

--- tools/events.py
+++ tools/events.py
@@ -53,7 +53,7 @@ class EventWrapper(Generic[_T]):
 
     """
     def __init__(self) -> None:
-        self.cache: dict[_T , Coro[_IN,RET]] = dict()
+        self.cache: dict[_T , Coro[...,None]] = dict()
 
     def __call__(self, key:Optional[_T] = None):
         """Wraps an asynchornous funciton the the cache"""
@@ -62,10 +62,11 @@ class EventWrapper(Generic[_T]):
                 raise TypeError(f"Function: {func.__name__!r} must be an asynchronous function")
 
             # One at a time!
-            if self.cache.get(key or func.__name__):
-                raise AttributeError(f"Key : {key or func.__name__!r} already has an assigned function called {self.cache[key or func.__name__]!r}")
+            _key = key or func.__name__
+            if self.cache.get(key): # type: ignore
+                raise AttributeError(f"Key : {_key!r} already has an assigned function called {self.cache[_key].__name__!r}")
             
-            self.cache[key or func.__name__] = func
+            self.cache[_key] = func # type: ignore
             return func 
         
         return decorator
@@ -129,108 +130,109 @@ class EventGroupWrapper(Generic[_T]):
         
 
 
-class TimedCondition(asyncio.Condition):
-    """A Condition that is a subclass of :class:`asyncio.Condition` but is made to act as a timeout before 
-    any execution being waited upon can run"""
-    
-    def __init__(self, delay:Union[int,float], lock:Optional[asyncio.Lock] = None) -> None:
-        super().__init__(lock=lock)
-
-        if delay < 0:
-            raise ValueError(f"default argument cannot use a number less than 0 but you chose {delay!r}")
-
-        self._delay = delay
-
-    @property
-    def delay(self):
-        """Obtains the delay you set up for use..."""
-        return self._delay
-
-    @delay.setter
-    def set_new_target_delay(self,delay:Union[int,float]):
-        """Used to setup new delays to the internal dealy variable
-        this will take effect whenever the :class:`TimedCondition` is not sleeping"""
-        if not isinstance(delay,(int,float)):
-            raise ValueError(f"delay argument must be a float or integer , got {type(delay)} from {delay!r}")
-        self._delay = delay
-        return self 
-
-    async def sleep(self, unlock_all:bool=True) -> None:
-        """Sleeps for amount of seconds chosen for the `.delay` argument and releases all tasks inside of it... 
-        Note that if notify all is called before this timer is over then it will be immediately relased 
-        and this timer will not matter at all, note that this is made not to cause a `RuntimeError` and it 
-        can therefore be bailed out...
-        
-        if `unlock_all` is True then `notify_all()` gets invoked otherwise `notify()` will be used intead
-        default is True"""
-        await asyncio.sleep(self._delay)
-        if self.locked():
-            self.notify_all() if unlock_all else self.notify()
 
 class RateLimitCondition:
-    """Used to help moderate a ratelimit.
-    A Greate Example Can be a class that is used to help an http client obey RateLimits and pevent  
-    Too many requests from going to the server at a time.
+    """Used to help moderate ratelimiting.
+    A Great Example Can be a class that is used to help an http client obey RateLimits and pevent  
+    the client from getting a `Too Many Requests` (429) response.
     
-    Note that this does not take into account the concurrency until the it 
-    has hit the limit and if the time hasn't cycled yet then it will be locked.
+    Note that this does not take into account the concurrency limit of whatever your site has set until the  
+    the limit has been hit and if the time hasn't cycled over yet then it will have to wait until the next interval can be called.
     it will then wait the timeout and then resume 
-    execution as normal once the cycles have cycled"""
+    execution as normal once the cycles have cycled
+
+    This Ratelimit-Condition can be used along-side aiohttp to keep 
+    track of a target website's rate-limit
+::
+
+    # 3 requests for every 15 seconds...
+    limit = RateLimitCondition(3,15)
+    # later in the code...
+    async with aiohttp.ClientSession("https://www.example.com") as client:
+        async with limit , client.get("/") as response:
+        ...
+    """
 
     async def _cycle(self):
-        # Run Forever!
-        while True:
-            async with self._timer_cond:
-                await self._timer_cond.sleep()
-                # a cycle was completed so reset the task limit back to 0
-                self._tl = 0
-        
+        while not self.is_killed:
+            await asyncio.sleep(self.interval)
+            # Allow the rest to leak through until another is required...
+            # Set all underlying tasks back to zero before calling so that 
+            # a roll-call of all pending tasks can now be called back up if 
+            # we have any that are still pending by removing the limit from the amount...
+            self.tasks = 0
+            self._timer.set()
+            # Clear afterwards and restart...
+            self._timer.clear()
+
+    def __init__(self,limit:int,interval:Union[float,int]) -> None:
+        self.limit = limit 
+        self.interval = interval
+        # NOTE I'm using an event since it's easier to moderate vs using a condition...
+        self._timer = asyncio.Event()
+        self.is_killed = False
+        self.pending:int = 0
+        """tracks all tasks that are still inside of `__aenter__()` before `__aexit__()` can be called
+        this tracker is used to wait for all tasks to end gracefully..."""
+
+        self.tasks:int = 0
+        """keeps track of all currently running tasks"""
+        self._inner_cycle = asyncio.create_task(self._cycle())
+
+    async def _add_task(self):
+        """acts as a lock to moderate tasks easily, so that when one rate-limit cycle is over another 
+        will be made and all pedning tasks that were blocked out are then let in..."""
+        if self.is_killed:
+            raise RuntimeError("RateLimit's Condition has already been killed off...")
+        self.tasks += 1
+        if self.tasks >= self.limit:
+            await self._timer.wait()
+            # all tasks needs re_adding...
+            await self._add_task()
+
     async def __aenter__(self):
-        """Note that this Value returns with nothing because it uses lockdown behaviors..."""
-        # if it is the first time running then invoke the _cycle ...
-        if not self._cycle_task:
-            self._cycle_task = asyncio.create_task(self._cycle())
+        """Used to act as a lock of it's own...
+        this is equivilent to waiting for a slot to be opened via `_add_task()`..."""
+        self.pending += 1
+        await self._add_task()
+            # Re-add all of our tasks that were being wated on since they've now been unlocked...
 
-        if self._killed:
-            raise RuntimeError(f"RateLimit {self.__name__!r} was already killed!")
+    async def __aexit__(self,*args):
+        # gets rid of the currently pending task...
+        self.pending -= 1
+        return 
 
-        await self._timer_cond.acquire()
-        if self._tl == self._task_limit:
-            await self._timer_cond.wait()
+    def stop(self):
+        """Stop rate_limit's neverending inner `self._cycle` raises a RuntimeError if there's any remaining tasks left"""
+        if self.pending != 0:
+            raise RuntimeError("stop() couldn't be called because there's still %i tasks left to be ran" % self.pending)
+        try:
+            self._inner_cycle.cancel()
+        finally:
+            self.is_killed = True 
+            return 
 
-        # add one to the counter 
-        self._tl += 1 
-        return None 
+    async def stop_gracefully(self):
+        """Wait for all remaining tasks to be done and then exit everything out and end the rate-limit..."""
+        while self.pending > 0:
+            await asyncio.sleep(0.005)
+        return self.stop()
     
-    async def __aexit__(self):
-        # Release the coroutine the we gave to the rate_limit 
-        self._timer_cond.release()
+    async def limited_function(self,func:Coro[_IN,RET]):
+        """Used as a drop in replacement wrapper for wrapping coroutines with a ratelimit to better 
+        control an outgoing rate-limit if needed... this can also work with def functions but 
+        can yeild side effects if your're not super careful about it and the return type of the
+        normal def funcion is not a future or task or coroutine afterwards...
+    ::
 
+        rate_limit = RateLimitCondition(3,60)
+        @rate_limit.limited_function
+        async def make_get_request(site:str):
+            ...
+        """
 
-    def __init__(self, task_limit:int, delay:int, lock:Optional[asyncio.Lock] = None) -> None:
-        self._tl = 0
-        """The task limit is an internal varibale that keeps track of how many 
-        tasks were executed in the time that it has left"""
-        self._task_limit = task_limit 
-        self._delay = delay 
-        self._timer_cond = TimedCondition(delay,lock)
-        self._cycle_task = None 
-        self._killed = False 
-
-
-    @property
-    def task_count(self):
-        """Returns back the number of tasks that have been executed within it's own iterval"""
-        return self._tl
+        async def decorator(*args,**kwargs):
+            async with self:
+                return await func(*args,**kwargs)
+        return decorator
     
-    def kill(self):
-        """Kills the ratelimit's inner cycle, this should only be done when you're no longer going to be 
-        interating with the target server for example...
-        WARNING! Once the Rate-Limit is killed, if there are still running tasks that are wating for a current cycle to end 
-        then they will all be freed!"""
-        self._timer_cond.notify_all()
-        if self._cycle_task:
-            self._cycle_task.cancel()
-        self._killed = True 
-        
-
